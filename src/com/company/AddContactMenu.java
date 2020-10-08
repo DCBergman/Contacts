4d66df737b1796e7f543f7866e56af3938edcfb5
Daniela Bergman <daniela.bergman@hotmail.com>
2020-10-08 11:05:22 +0200

    Added menu to add contact

--- src/com/company/AddContactMenu.java
+++ src/com/company/AddContactMenu.java
@@ -1,4 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class AddContactMenu {
+    private static AddContactMenu instance = null;
+
+    private AddContactMenu() {
+    }
+
+    public static AddContactMenu getInstance() {
+        if (instance == null) {
+            instance = new AddContactMenu();
+        }
+        return instance;
+    }
+
+    public String showMenuAndGetChoice() {
+
+        System.out.println("\nPlease select type of contact:  \n----------");
+
+        String[] contactTypes ={"Friend", "Family", "Business"};
+        int i = 1;
+        for (String contactType: contactTypes) {
+            System.out.println(i + ". " + contactType);
+            i++;
+        }
+        Scanner scanner = new Scanner(System.in);
+        int choiceIndex = scanner.nextInt();
+        return contactTypes[choiceIndex-1];
+
+    }
 }
